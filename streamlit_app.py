e99df622ea34816f70aba445ba327b9705f177ab
Anoop Kumar U <anooprockstar10@gmail.com>
2025-06-05 15:04:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ if st.button("🔊 Generate and Play Audio"):
     audio_gen = elevenlabs.text_to_speech.convert(
         text=text,
         voice_id="nPczCjzI2devNBz1zQrb",
-        model_id="eleven_multilingual_v2",
+        model_id="eleven_flash_v2.5",
         output_format="mp3_44100_128"
     )
 
